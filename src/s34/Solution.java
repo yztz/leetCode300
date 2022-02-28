9aa7680fccd93ae65128d57029a7e6aebc8deba5
aikhi007 <781089956@qq.com>
2022-02-28 20:09:49 +0800

    s34☆☆☆枚举题~二月勋章我来辣!

---
+++ src/s34/Solution.java
@@ -0,0 +1,37 @@
+package s34;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int maximumRequests(int n, int[][] requests) {
+        int[] delta = new int[n];
+        int ans = 0, m = requests.length;
+        for (int mask = 0; mask < (1 << m); ++mask) {
+            int cnt = Integer.bitCount(mask);
+            if (cnt <= ans) {
+                continue;
+            }
+            Arrays.fill(delta, 0);
+            for (int i = 0; i < m; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    ++delta[requests[i][0]];
+                    --delta[requests[i][1]];
+                }
+            }
+            boolean flag = true;
+            for (int x : delta) {
+                if (x != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                ans = cnt;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
